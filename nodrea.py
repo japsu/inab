df28f124ad6dcfc6016b9158d526f36c410a1414
Santtu Pajukanta <santtu@pajukanta.fi>
2023-04-24 14:28:15 +0300

    fix: aforementioned problem with dates, add example

--- nodrea.py
+++ nodrea.py
@@ -13,8 +13,7 @@ class TransactionType(str, Enum):
 
 
 class State(str, Enum):
-    DATE = r"^(?P<date>\d{1,2}\.\d{1,2}\.\d{4})$"
-    DESCRIPTION = r"^(?P<type>Osto|Suoritus|Hyvitys) (?P<description>.+)"
+    DATE_OR_DESCRIPTION = r"^((?P<date>\d{1,2}\.\d{1,2}\.\d{4})|(?P<type>Osto|Suoritus|Hyvitys) (?P<description>.+))$"
     SUM = r"^(?P<minus>−)?(?P<euros>\d+),(?P<cents>\d{2})$"
 
 
@@ -26,7 +25,7 @@ class CreditCardTransaction(BaseModel):
 
     @classmethod
     def from_nodrea(cls, lines):
-        state = State.DATE
+        state = State.DATE_OR_DESCRIPTION
         date_ = date(1970, 1, 1)
         type = TransactionType.UNKNOWN
         description = ""
@@ -34,14 +33,14 @@ class CreditCardTransaction(BaseModel):
         for line in lines:
             if match := re.match(state.value, line, re.UNICODE):
                 match state:
-                    case State.DATE:
-                        date_ = datetime.strptime(match.group("date"), "%d.%m.%Y").date()
-                        state = State.DESCRIPTION
-
-                    case State.DESCRIPTION:
-                        type = TransactionType(match.group("type"))
-                        description = match.group("description")
-                        state = State.SUM
+                    case State.DATE_OR_DESCRIPTION:
+                        if date_str := match.group("date"):
+                            # there may be 1 to 3 dates, the last of which is the interesting one
+                            date_ = datetime.strptime(match.group("date"), "%d.%m.%Y").date()
+                        elif type_str := match.group("type"):
+                            type = TransactionType(type_str)
+                            description = match.group("description")
+                            state = State.SUM
 
                     case State.SUM:
                         cents = 100 * int(match.group("euros")) + int(match.group("cents"))
@@ -50,7 +49,7 @@ class CreditCardTransaction(BaseModel):
 
                         yield cls(type=type, date=date_, description=description, cents=cents)
 
-                        state = State.DATE
+                        state = State.DATE_OR_DESCRIPTION
                         date_ = date(1970, 1, 1)
                         type = TransactionType.UNKNOWN
                         description = ""
@@ -60,7 +59,6 @@ class CreditCardTransaction(BaseModel):
 
 
 if __name__ == "__main__":
-    from sys import stdin
-
-    for txn in CreditCardTransaction.from_nodrea(stdin.readlines()):
-        print(txn)
+    with open("examples/credit_card_copypasta.txt", encoding="UTF-8") as input_file:
+        for txn in CreditCardTransaction.from_nodrea(input_file.readlines()):
+            print(txn)
